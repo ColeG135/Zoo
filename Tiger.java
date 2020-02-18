8259d11589f726aa7c14bbe8d6c3a2d2070d97d0
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2020-02-18 13:35:24 -0800

    Update Tiger.java

--- Tiger.java
+++ Tiger.java
@@ -1,1 +1,14 @@
-
+public class Tiger {
+    public Tiger (String Tigger) {
+    }
+    public void sleep() {
+    }
+    public void eat (String food) {
+        food = "meat";
+    }
+    public static void main(String[] args) {
+        Tiger tigger = new Tiger("Tigger");
+        tigger.eat("meat");
+        tigger.eat("kibble");
+    }
+}
