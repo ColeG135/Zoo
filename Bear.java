71ba2bfa0bfa6da47c713d2275f51246c6a591e7
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2020-02-18 13:35:45 -0800

    Update Bear.java

--- Bear.java
+++ Bear.java
@@ -1,1 +1,13 @@
-
+public class Bear {
+    public Bear (String Pooh) {
+    }
+    public void sleep() {
+    }
+    public void eat (String food) {
+        food = "Fish";
+    }
+    public static void main(String[] args) {
+        Bear Pooh = new Bear( "Pooh");
+        Pooh.eat("Fish");
+    }
+}
