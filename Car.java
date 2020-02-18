2e8a894a2d7c90de860a6e6600a1939e83bd1bd9
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2020-02-18 13:34:59 -0800

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,1 +1,23 @@
+public class Car {
+    //properties
+    int year;
+    String model;
+    //constructor
+    public Car(int yearVar, String carModel) {
+        this.year = yearVar;
+        this.model = carModel;
+    }
+    //method
+    public void printCarInfo() {
+        System.out.println("I am a car of model" +
+                this.model + "and year: " + this.year);
+    }
+    Car x = new Car(1992, "Hundai");
+    Car y = new Car(1956, "Toyota");
+
+
+
+//    y.printCarInfo();
+//    x.printCarInfo();
+}
 
