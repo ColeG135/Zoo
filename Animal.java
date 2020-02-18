2031b28982a7efa3cc20427710e7d283b7601271
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2020-02-18 14:05:04 -0800

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,11 +1,16 @@
 class Animal {
     public Animal(String Tiger, String Bacon) {
-
+        
     }
-    public void sleep() {
-
+    public void sleep(String name) {
+        System.out.print(name +
+        "sleeps for 8 hours");
     }
-    public void eat() {
-
+    public void eat(String food) {
+        Bear Pooh = new Bear("Pooh");
+        Pooh.eat("fish");
+        Tiger tigger = new Tiger("Tigger");
+        tigger.eat( "meat");
+        tigger.eat("kibble");
     }
 }
