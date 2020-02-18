9c7bb92a37628f17753378b2e91f1d80fc8eece1
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2020-02-18 13:34:35 -0800

    Update Zoo.java

--- Zoo.java
+++ Zoo.java
@@ -1,1 +1,12 @@
+public class Zoo {
+    public static void main(String args) {
+        Zoo z = new Zoo();
+        z.sleep("Tigger");
+    }
+    public void sleep(String name) {
+        System.out.print(name + "sleeps for 8 hours");
+    }
 
+    String favoriteFood = "bacon";
+
+}
