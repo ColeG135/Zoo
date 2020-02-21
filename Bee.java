255ca047827390e7b0a242bd8dad70d58e280d64
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2020-02-21 14:03:23 -0800

    Update and rename Bee to Bee.java

---
+++ Bee.java
@@ -0,0 +1,13 @@
+public class Bee {
+    public Bee (String Stinger) {
+    }
+    public void sleep() {
+    }
+    public void eat (String food) { food = "pollen"; }
+    public static void main(String[] args) {
+        Bee Stinger = new Bee("Stinger");
+        Stinger.eat("Honey");
+    }
+    String favoriteFood = "nector";
+}
+
