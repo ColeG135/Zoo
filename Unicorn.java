f75318f366e5b358b79cf47ab90d455088de91a0
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2020-02-21 14:04:32 -0800

    Create Unicorn.java

---
+++ Unicorn.java
@@ -0,0 +1,12 @@
+public class Unicorn {
+    public Unicorn (String Rarity) {
+    }
+    public void sleep() {
+    }
+    public void eat (String food) { food = "confetti"; }
+    public static void main(String[] args) {
+        Unicorn Rarity = new Unicorn("Rarity");
+        Rarity.eat("confetti");
+    }
+    String favoriteFood = "rainbows";
+}
