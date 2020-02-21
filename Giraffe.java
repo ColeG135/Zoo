7e4173d780c80545263ab370dc30da6d5146ca01
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2020-02-21 14:04:00 -0800

    Update Giraffe.java

--- Giraffe.java
+++ Giraffe.java
@@ -1,1 +1,12 @@
-
+public class Giraffe {
+    public Giraffe (String Gemma) {
+    }
+    public void sleep() {
+    }
+    public void eat (String food) {food = "grass"; }
+    public static void main(String[] args) {
+        Giraffe Gemma = new Giraffe(" Gemma");
+        Gemma.eat("grass");
+    }
+    String favoriteFood = "hay";
+}
